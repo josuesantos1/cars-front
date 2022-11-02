65b36d2f4f0e3fd9cf45c0041c23d27fdc26fc43
Josué Santos <josue.santos.dev@gmail.com>
2022-11-02 10:48:28 -0300

    env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    URL: 'http://localhost:3030',
+  }
 }
 
 module.exports = nextConfig
