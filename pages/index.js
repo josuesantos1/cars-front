baa65d315f6eb6120102e85edbd386312761ccec
Josué Santos <josue.santos.dev@gmail.com>
2022-11-02 10:47:48 -0300

    index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import * as Cars from '../api/cars'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [cars, setCars] = useState()
+
+  useEffect(() => {
+    const result = Cars.default.Listing()
+    console.log(cars)
+    setCars(result)
+    console.log(typeof cars)
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,42 +26,12 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {cars?.map(car => {
+            return <div className={styles.card}>
+              {car.title}
+            </div>
+          })}
         </div>
       </main>
 
